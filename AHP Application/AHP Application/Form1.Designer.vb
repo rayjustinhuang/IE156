fd0d12b661d22ff3b6f9ca5d6a5ac1844520557b
rayjustinhuang <rayjustinhuang@gmail.com>
2017-03-02 00:01:04 +0800

    Complete first case study - create simple AHP application

---
+++ AHP Application/AHP Application/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtncriteria = New System.Windows.Forms.TextBox()
+        Me.txtnalternatives = New System.Windows.Forms.TextBox()
+        Me.btngenerate = New System.Windows.Forms.Button()
+        Me.dgvpcm = New System.Windows.Forms.DataGridView()
+        Me.dgvnormalizedpcm = New System.Windows.Forms.DataGridView()
+        Me.btnnormalize = New System.Windows.Forms.Button()
+        Me.dgvfinaldec = New System.Windows.Forms.DataGridView()
+        Me.btncalfinaldec = New System.Windows.Forms.Button()
+        Me.btninitialize = New System.Windows.Forms.Button()
+        CType(Me.dgvpcm, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvnormalizedpcm, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvfinaldec, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Number of Criteria"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(142, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter Number of Alternatives"
+        '
+        'txtncriteria
+        '
+        Me.txtncriteria.AccessibleName = "txtncriteria"
+        Me.txtncriteria.Location = New System.Drawing.Point(194, 34)
+        Me.txtncriteria.Name = "txtncriteria"
+        Me.txtncriteria.Size = New System.Drawing.Size(100, 20)
+        Me.txtncriteria.TabIndex = 2
+        '
+        'txtnalternatives
+        '
+        Me.txtnalternatives.AccessibleName = "txtnalternatives"
+        Me.txtnalternatives.Location = New System.Drawing.Point(194, 78)
+        Me.txtnalternatives.Name = "txtnalternatives"
+        Me.txtnalternatives.Size = New System.Drawing.Size(100, 20)
+        Me.txtnalternatives.TabIndex = 3
+        '
+        'btngenerate
+        '
+        Me.btngenerate.AccessibleName = "btngenerate"
+        Me.btngenerate.Location = New System.Drawing.Point(173, 120)
+        Me.btngenerate.Name = "btngenerate"
+        Me.btngenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btngenerate.TabIndex = 4
+        Me.btngenerate.Text = "Generate"
+        Me.btngenerate.UseVisualStyleBackColor = True
+        '
+        'dgvpcm
+        '
+        Me.dgvpcm.AllowUserToAddRows = False
+        Me.dgvpcm.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvpcm.Location = New System.Drawing.Point(49, 162)
+        Me.dgvpcm.Name = "dgvpcm"
+        Me.dgvpcm.Size = New System.Drawing.Size(460, 245)
+        Me.dgvpcm.TabIndex = 5
+        '
+        'dgvnormalizedpcm
+        '
+        Me.dgvnormalizedpcm.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvnormalizedpcm.Location = New System.Drawing.Point(516, 162)
+        Me.dgvnormalizedpcm.Name = "dgvnormalizedpcm"
+        Me.dgvnormalizedpcm.Size = New System.Drawing.Size(460, 245)
+        Me.dgvnormalizedpcm.TabIndex = 6
+        '
+        'btnnormalize
+        '
+        Me.btnnormalize.Location = New System.Drawing.Point(616, 117)
+        Me.btnnormalize.Name = "btnnormalize"
+        Me.btnnormalize.Size = New System.Drawing.Size(75, 23)
+        Me.btnnormalize.TabIndex = 7
+        Me.btnnormalize.Text = "Normalize"
+        Me.btnnormalize.UseVisualStyleBackColor = True
+        '
+        'dgvfinaldec
+        '
+        Me.dgvfinaldec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvfinaldec.Location = New System.Drawing.Point(49, 461)
+        Me.dgvfinaldec.Name = "dgvfinaldec"
+        Me.dgvfinaldec.Size = New System.Drawing.Size(927, 245)
+        Me.dgvfinaldec.TabIndex = 8
+        '
+        'btncalfinaldec
+        '
+        Me.btncalfinaldec.AccessibleName = "btngenerate"
+        Me.btncalfinaldec.Location = New System.Drawing.Point(280, 423)
+        Me.btncalfinaldec.Name = "btncalfinaldec"
+        Me.btncalfinaldec.Size = New System.Drawing.Size(145, 23)
+        Me.btncalfinaldec.TabIndex = 9
+        Me.btncalfinaldec.Text = "Calculate Final Decision"
+        Me.btncalfinaldec.UseVisualStyleBackColor = True
+        '
+        'btninitialize
+        '
+        Me.btninitialize.AccessibleName = "btngenerate"
+        Me.btninitialize.Location = New System.Drawing.Point(173, 423)
+        Me.btninitialize.Name = "btninitialize"
+        Me.btninitialize.Size = New System.Drawing.Size(75, 23)
+        Me.btninitialize.TabIndex = 10
+        Me.btninitialize.Text = "Initialize"
+        Me.btninitialize.UseVisualStyleBackColor = True
+        '
+        'frmmain
+        '
+        Me.AccessibleName = "frmmain"
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.Controls.Add(Me.btninitialize)
+        Me.Controls.Add(Me.btncalfinaldec)
+        Me.Controls.Add(Me.dgvfinaldec)
+        Me.Controls.Add(Me.btnnormalize)
+        Me.Controls.Add(Me.dgvnormalizedpcm)
+        Me.Controls.Add(Me.dgvpcm)
+        Me.Controls.Add(Me.btngenerate)
+        Me.Controls.Add(Me.txtnalternatives)
+        Me.Controls.Add(Me.txtncriteria)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmmain"
+        Me.Text = "Main GUI"
+        CType(Me.dgvpcm, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvnormalizedpcm, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvfinaldec, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtncriteria As TextBox
+    Friend WithEvents txtnalternatives As TextBox
+    Friend WithEvents btngenerate As Button
+    Friend WithEvents dgvpcm As DataGridView
+    Friend WithEvents dgvnormalizedpcm As DataGridView
+    Friend WithEvents btnnormalize As Button
+    Friend WithEvents dgvfinaldec As DataGridView
+    Friend WithEvents btncalfinaldec As Button
+    Friend WithEvents btninitialize As Button
+End Class
---
+++ AHP Application/AHP Application/Form1.vb
@@ -0,0 +1,158 @@
+﻿Public Class frmmain
+
+    Private Sub btngenerate_Click(sender As Object, e As EventArgs) Handles btngenerate.Click
+        If CheckForWrongInput() = False Then
+            Exit Sub
+        End If
+        PopulateDGVPCM()
+    End Sub
+
+    Private Function CheckForWrongInput()
+        If IsNumeric(txtncriteria.Text) = False Or IsNumeric(txtnalternatives.Text) = False Then
+            MsgBox("Please enter a valid positive integer")
+            Return False
+        ElseIf txtncriteria.Text <= 0 Or txtnalternatives.Text <= 0 Then
+            MsgBox("Please enter a valid positive integer greater than 0")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Sub PopulateDGVPCM()
+        For i As Integer = 0 To txtncriteria.Text
+            dgvpcm.Columns.Add(DGVNewColumn)
+            dgvpcm.Columns(i).SortMode = DataGridViewColumnSortMode.NotSortable
+            dgvpcm.Rows.Add()
+        Next
+        For i As Integer = 1 To txtncriteria.Text
+            dgvpcm.Rows(i).Cells(i).Value = 1
+        Next
+    End Sub
+
+    Private Function DGVNewColumn()
+        Dim blanktextcolumn As New DataGridViewTextBoxColumn
+        Return blanktextcolumn
+    End Function
+
+    Private Sub dgvpcm_CellValueChanged(sender As Object, e As DataGridViewCellEventArgs) Handles dgvpcm.CellValueChanged
+        If e.RowIndex = 0 Or e.ColumnIndex = 0 Then
+            If e.RowIndex = 0 And e.ColumnIndex > 0 Then
+                dgvpcm.Rows(e.ColumnIndex).Cells(0).Value = dgvpcm.Rows(0).Cells(e.ColumnIndex).Value
+            ElseIf e.ColumnIndex = 0 And e.RowIndex > 0 Then
+                dgvpcm.Rows(0).Cells(e.RowIndex).Value = dgvpcm.Rows(e.RowIndex).Cells(0).Value
+            End If
+        ElseIf e.RowIndex > 0 And e.ColumnIndex > 0 Then
+            If IsNumeric(dgvpcm.Rows(e.RowIndex).Cells(e.ColumnIndex).Value) = False Then
+                dgvpcm.Rows(e.RowIndex).Cells(e.ColumnIndex).Value = ""
+                MsgBox("Please enter a numeric positive number")
+                Exit Sub
+            ElseIf dgvpcm.Rows(e.RowIndex).Cells(e.ColumnIndex).Value < 0 Then
+                dgvpcm.Rows(e.RowIndex).Cells(e.ColumnIndex).Value = ""
+                MsgBox("Please enter a numeric positive number")
+                Exit Sub
+            End If
+            dgvpcm.Rows(e.ColumnIndex).Cells(e.RowIndex).Value = 1 / dgvpcm.Rows(e.RowIndex).Cells(e.ColumnIndex).Value
+        End If
+        dgvpcm.Rows(0).Cells(0).Value = ""
+    End Sub
+
+    Private Sub btnnormalize_Click(sender As Object, e As EventArgs) Handles btnnormalize.Click
+        PopulateDGVNormalizedPCM()
+        NormalizePCM()
+    End Sub
+
+    Private Sub PopulateDGVNormalizedPCM()
+        For i As Integer = 0 To txtncriteria.Text + 1
+            dgvnormalizedpcm.Columns.Add(DGVNewColumn)
+            dgvnormalizedpcm.Columns(i).SortMode = DataGridViewColumnSortMode.NotSortable
+            dgvnormalizedpcm.Rows.Add()
+        Next
+        For i As Integer = 0 To txtncriteria.Text
+            dgvnormalizedpcm.Rows(0).Cells(i).Value = dgvpcm.Rows(0).Cells(i).Value
+        Next
+        For i As Integer = 0 To txtncriteria.Text
+            dgvnormalizedpcm.Rows(i).Cells(0).Value = dgvpcm.Rows(i).Cells(0).Value
+        Next
+        dgvnormalizedpcm.Rows(txtncriteria.Text + 1).Cells(0).Value = "Sum"
+        Dim maxcol As Integer = txtncriteria.Text + 1
+        dgvnormalizedpcm.Rows(0).Cells(maxcol).Value = "Average"
+    End Sub
+
+    Private Sub NormalizePCM()
+        Dim currentsum As Double = 0
+        Dim maxcolumn As Integer = txtncriteria.Text + 1
+        For i As Integer = 1 To txtncriteria.Text
+            For j As Integer = 1 To txtncriteria.Text
+                currentsum = currentsum + dgvpcm.Rows(j).Cells(i).Value
+            Next
+            dgvnormalizedpcm.Rows(maxcolumn).Cells(i).Value = currentsum
+            currentsum = 0
+        Next
+        For i As Integer = 1 To txtncriteria.Text
+            For j As Integer = 1 To txtncriteria.Text
+                dgvnormalizedpcm.Rows(i).Cells(j).Value = dgvpcm.Rows(i).Cells(j).Value / dgvnormalizedpcm.Rows(maxcolumn).Cells(j).Value
+            Next
+        Next
+        currentsum = 0
+        For i As Integer = 1 To txtncriteria.Text
+            For j As Integer = 1 To txtncriteria.Text
+                currentsum = currentsum + dgvnormalizedpcm.Rows(i).Cells(j).Value
+            Next
+            dgvnormalizedpcm.Rows(i).Cells(maxcolumn).Value = currentsum / txtncriteria.Text
+            currentsum = 0
+        Next
+    End Sub
+
+    Private Sub btninitialize_Click(sender As Object, e As EventArgs) Handles btninitialize.Click
+        PopulateDGVFinalDec
+        PopulateCriteriaandWeights
+    End Sub
+
+    Private Sub PopulateDGVFinalDec()
+        For i As Integer = 0 To txtncriteria.Text + 1
+            dgvfinaldec.Columns.Add(DGVNewColumn)
+            dgvfinaldec.Columns(i).SortMode = DataGridViewColumnSortMode.NotSortable
+        Next
+        For i As Integer = 0 To txtnalternatives.Text + 1
+            dgvfinaldec.Rows.Add()
+        Next
+    End Sub
+
+    Private Sub PopulateCriteriaandWeights()
+        Dim maxcolumn As Integer = txtncriteria.Text + 1
+        For i As Integer = 1 To txtncriteria.Text
+            dgvfinaldec.Rows(0).Cells(i).Value = dgvpcm.Rows(0).Cells(i).Value
+        Next
+        For i As Integer = 1 To txtncriteria.Text
+            dgvfinaldec.Rows(1).Cells(i).Value = dgvnormalizedpcm.Rows(i).Cells(maxcolumn).Value
+        Next
+    End Sub
+
+    Private Sub dgvfinaldec_CellValueChanged(sender As Object, e As DataGridViewCellEventArgs) Handles dgvfinaldec.CellValueChanged
+        If e.RowIndex > 1 And e.ColumnIndex > 0 Then
+            If IsNumeric(dgvfinaldec.Rows(e.RowIndex).Cells(e.ColumnIndex).Value) = False Then
+                MsgBox("Please enter a positive number")
+                dgvfinaldec.Rows(e.RowIndex).Cells(e.ColumnIndex).Value = ""
+                Exit Sub
+            ElseIf dgvfinaldec.Rows(e.RowIndex).Cells(e.ColumnIndex).Value < 0 Then
+                MsgBox("Please enter a positive number")
+                dgvfinaldec.Rows(e.RowIndex).Cells(e.ColumnIndex).Value = ""
+                Exit Sub
+            End If
+        End If
+    End Sub
+
+    Private Sub btncalfinaldec_Click(sender As Object, e As EventArgs) Handles btncalfinaldec.Click
+        Dim currentsum As Double = 0
+        Dim maxcolumn As Integer = txtncriteria.Text + 1
+        For i As Integer = 2 To txtnalternatives.Text + 1
+            For j As Integer = 1 To txtncriteria.Text
+                currentsum = currentsum + dgvfinaldec.Rows(i).Cells(j).Value * dgvfinaldec.Rows(1).Cells(j).Value
+            Next
+            dgvfinaldec.Rows(i).Cells(maxcolumn).Value() = currentsum
+            currentsum = 0
+        Next
+    End Sub
+
+End Class
